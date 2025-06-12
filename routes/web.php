37097e367b4644a12bb01fa5b22040b8cd3f870c
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-06-12 14:10:41 +0100

    Ajout des routes d'inscription et de connexion, ainsi que des groupes de routes protégés (admin, médecin, patient)

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+
+
+Route::middleware('guest')->group(function () {
 Route::get('/', function () {
     return view('welcome');
 });
+
+
 
 Route::get('/inscription', [AuthController::class, 'showRegisterForm'])->name('inscription');
 Route::post('/inscription', [AuthController::class, 'register'])->name('register');
@@ -13,7 +18,21 @@ Route::post('/inscription', [AuthController::class, 'register'])->name('register
 Route::get('/login', [AuthController::class, 'showloginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
+});
 
-Route::get('/profil ', function(){return view('patients.profil.index');});
-Route::get('/Historique ', function(){return view('patients.Historique');});
-Route::get('/rendez-vous ', function(){return view('patients.rendez-vous');});
+
+Route::middleware([ 'auth','isadmin'])->group(function () {
+    // Routes des admins
+});
+
+Route::middleware(['auth','ismedecin'])->group(function () {
+    // Routes des médecins
+});
+
+Route::middleware(['auth','ispatient'])->group(function () {
+
+});
+
+    Route::get('/profil ', function(){return view('patients.profil.index');});
+    Route::get('/Historique ', function(){return view('patients.Historique');});
+    Route::get('/rendez-vous ', function(){return view('patients.rendez-vous');});
