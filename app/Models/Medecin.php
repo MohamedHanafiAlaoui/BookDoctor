2449aecc8cc1027d45fefd488b776de2adbee694
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-06-22 23:13:24 +0100

    Création du modèle Medecin avec champs personnalisés et relations avec User et Specialite

--- app/Models/Medecin.php
+++ app/Models/Medecin.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Medecin extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey = 'id'; // Puisque tu as utilisé id (foreign key liée à users)
+    public $incrementing = false;
+    protected $keyType = 'unsignedBigInteger';
+
+    protected $fillable = [
+        'id',
+        'license_number',
+        'years_of_experience',
+        'image',
+        'adresse',
+        'description',
+        'specialite_id',
+    ];
+
+    // Relation avec l'utilisateur (user)
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id');
+    }
+
+    // Relation avec la spécialité
+    public function specialite()
+    {
+        return $this->belongsTo(Specialite::class, 'specialite_id');
+    }
 }
