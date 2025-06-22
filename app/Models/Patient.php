ea0eaef60983b23ecd097bb907431e36df74f107
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-06-22 23:14:37 +0100

    Création du modèle Patient avec relations et champs médicaux personnalisés

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,14 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
 {
-    protected $fillable = [
-    'id_user', 
-    'medical_history',
-    'image',
-];
+    use HasFactory;
 
+    protected $primaryKey = 'id_user'; // Puisque ta clé primaire est id_user
+    public $incrementing = false; // Car id_user est aussi une foreign key et non auto-incrémentée
+    protected $keyType = 'unsignedBigInteger'; // Type de la clé primaire
+
+    protected $fillable = [
+        'id_user',
+        'medical_history',
+        'groupe_sanguin',
+        'allergies',
+        'adresse',
+        'code_postal',
+        'ville',
+        'image',
+    ];
+
+    // Relation avec l'utilisateur
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
 }
