29e9c636040f5f85e1c98073275943ae46aee6c9
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-06-25 11:33:15 +0100

    Create rendez_vouses table

---
+++ database/migrations/2025_06_25_102045_create_rendez_vouses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rendez_vouses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('medecin_id')->constrained('users')->onDelete('cascade');
+            $table->date('date_rendez_vous');
+            $table->time('heure');
+            $table->string('motif')->nullable();
+            $table->enum('statut', ['en attente', 'confirmé', 'annulé'])->default('en attente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rendez_vouses');
+    }
+};
